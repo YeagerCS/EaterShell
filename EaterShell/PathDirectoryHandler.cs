93b2f9fc1fdbe1e3ad596b916aa488c8146b82de
Debug <lejsmalisi0@gmail.com>
2023-11-07 09:11:45 +0100

    Sneaker

--- EaterShell/PathDirectoryHandler.cs
+++ EaterShell/PathDirectoryHandler.cs
@@ -12,7 +12,18 @@ namespace EaterShell
         private static string currentDirectory;
         private static TheDirectory _currentDirectory;
         private static Drive selectedDrive;
+        private static TheDirectory tempDirectory;
 
+
+        public static TheDirectory GetTempDirectory()
+        {
+            return tempDirectory;
+        }
+
+        public static void SetTempDirectory(TheDirectory directory)
+        {
+            tempDirectory = directory;
+        }
 
         public static void SelectDrive(Drive drive)
         {
---
+++ EaterShell/TypeCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EaterShell
+{
+    public class TypeCommand : Command
+    {
+        public override string Name => "type";
+
+        public override void Execute()
+        {
+            string filename = Parameters[0];
+            if (!filename.Contains("."))
+            {
+                OutputWriter.WriteLine(filename + " is not a valid file");
+            }
+            else
+            {
+                PathHandler pathHandler = new(PathDirectoryHandler.GetCurrentDirectory());
+                TheFile meantFile = pathHandler.SearchFileSystemItem(filename) as TheFile;
+                if (meantFile != null)
+                {
+                    OutputWriter.WriteLine(meantFile.Content);
+                }
+                else
+                {
+                    OutputWriter.WriteLine(filename + " is not a valid file");
+                }
+            }
+        }
+    }
+}
