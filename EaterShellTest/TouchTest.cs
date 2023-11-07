e7f4881590acf0a7841d95a60ba1254646553c7d
KaanK <citrusshooter1980@gmail.com>
2023-11-07 14:20:33 +0100

    mkdir Test + TouchTest

---
+++ EaterShellTest/TouchTest.cs
@@ -0,0 +1,31 @@
+﻿using EaterShell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EaterShellTest
+{
+    [TestClass]
+    public class TouchTest
+    {
+        [TestMethod]
+        public void FileeateatDoesNotExist_InputEqualseateat_CreateFileeateat()
+        {
+            // Arrange
+            ShellWorkspace shellWorkspace = new ShellWorkspace();
+            PathHandler pathHandler = new PathHandler(PathDirectoryHandler.GetCurrentDirectory());
+            TouchCommand touchCommand = new TouchCommand();
+            string newFile = "eat.eat";
+
+            // Act
+            touchCommand.Parameters = new string[] { newFile, "" };
+            touchCommand.Execute();
+
+            // Assert
+            FileSystemItem file = pathHandler.SearchFileSystemItem(PathDirectoryHandler.GetCurrentDirectory + "\\" + newFile);
+            Assert.IsNotNull(file);
+        }
+    }
+}
